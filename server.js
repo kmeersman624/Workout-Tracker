ef7614d5c9bf321488941fda6f728274a6de4c56
kmeersman <katie4682@yahoo.com>
2020-06-26 10:22:18 -0600

    fixed routes in server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false,
 });
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 
 app.listen(PORT, function () {
   console.log(`App listening on Port ${PORT}`);
