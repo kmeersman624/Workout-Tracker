0cc23a49b15fb2e4af4a1c30152416c5e2049761
kmeersman <katie4682@yahoo.com>
2020-06-26 10:15:40 -0600

    updated server.js

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const app = express();
 const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(morgan("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
